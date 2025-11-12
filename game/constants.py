afc76f954ea11145c375aff0be281a34566f2961
Mystic1127 <mystic11gamer@gmail.com>
2025-11-12 16:02:16 -0500

    Update constants.py

--- game/constants.py
+++ game/constants.py
@@ -1,8 +1,5 @@
-"""Constants used throughout the Pac-Man game."""
-
 from __future__ import annotations
 
-# Screen settings
 TILE_SIZE = 20
 GRID_WIDTH = 28
 GRID_HEIGHT = 31
@@ -10,7 +7,6 @@ SCREEN_WIDTH = GRID_WIDTH * TILE_SIZE
 SCREEN_HEIGHT = GRID_HEIGHT * TILE_SIZE
 FRAME_DELAY_MS = 16  # ~60 FPS
 
-# Colors expressed as Tk-compatible hex strings
 Color = str
 
 BLACK: Color = "#000000"
@@ -24,7 +20,6 @@ ORANGE: Color = "#FFA500"
 BLUE_GRAY: Color = "#1F1F3A"
 PELLET_YELLOW: Color = "#FFE07D"
 
-# Map layout: '#' wall, '.' pellet, 'o' power pellet, ' ' empty, 'P' player start, 'G' ghost start
 MAZE_LAYOUT = [
     "############################",
     "#............##............#",
@@ -56,5 +51,4 @@ MAZE_LAYOUT = [
     "############################",
 ]
 
-# Ghost colors used sequentially
 GHOST_COLORS = [RED, CYAN, PINK, ORANGE]
